ee056e4fa69c1a32fb114758eddec4a4565cb338
NikSg02 <63389091+NikSg02@users.noreply.github.com>
2020-12-06 21:56:06 +0400

    Add files via upload

---
+++ регистр/Reg/RegForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Reg
+{
+    partial class RegForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.nameLabel.Location = new System.Drawing.Point(0, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(494, 21);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "label1";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // RegForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 322);
+            this.Controls.Add(this.nameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "RegForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reg";
+            this.Load += new System.EventHandler(this.RegForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
+
---
+++ регистр/Reg/RegForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Reg
+{
+    public partial class RegForm : Form
+    {
+        RegistrationForm registrationForm = new RegistrationForm();
+
+        public RegForm()
+        {
+            InitializeComponent();
+
+            registrationForm.ShowDialog(); // Отображаем форму авторизации.
+
+            nameLabel.Text = "Здравствуйте, " + registrationForm.login + "!";
+        }
+
+        private void RegForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
